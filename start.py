326f2375088b222e7b593d753ec1fd2e37e28d63
RMO <rmo@sitaniec.pl>
2024-01-31 19:54:52 +0100

    po pierwszym dniu

--- start.py
+++ start.py
@@ -1,7 +1,13 @@
 import xml.etree.ElementTree as Xet 
 import pandas as pd 
 
-cols = ["name","id","parent"]
+cols = ["name","id","ref","ref_name","ref_type","asm"]
+# name - nazwa użyta z sufixem :kolejne_wystapienie
+# id - id użycia
+# ref - id referencji (źródła) 1:n (n użyć)
+# ref_name - nazwa referncji
+# ref_type - typ referencji (solid, assembly)
+# asm - id assembly (tu trzeba będzie obrócić bo to asm mają listę części a mi trzeba )
 rows = []
 
 #parsing xml
@@ -9,17 +15,31 @@ xmlparse = Xet.parse('test.xml')
 root = xmlparse.getroot()
 #print(root)
 maszyna = xmlparse.find('{http://www.plmxml.org/Schemas/PLMXMLSchema}ProductDef/{http://www.plmxml.org/Schemas/PLMXMLSchema}InstanceGraph')
-print(maszyna)
+#print(maszyna)
 print("\n")
 skladowe = list(maszyna.findall('{http://www.plmxml.org/Schemas/PLMXMLSchema}Instance'))
-print(skladowe)
+referencje = list(maszyna.findall('{http://www.plmxml.org/Schemas/PLMXMLSchema}ProductRevisionView'))
+print(referencje)
 for i in skladowe: 
     name=i.attrib["name"]
     id=i.attrib["id"]
-    parent=i.attrib[""]
+    ref=i.attrib["partRef"]
+    for x in referencje:
+        ref_name = "x"
+        ref_type = "x"
+        asm = "x"
+        if x.attrib["id"] == ref[1:]:
+            ref_name = x.attrib["name"]
+            ref_type = x.attrib["type"]
+            if ref_type == "assembly":
+                asm = x.attrib["instanceRefs"]
     rows.append({"name": name,
-                 "id": id,
-                 "parent": parent})
+                  "id": id,
+                  "ref": ref[1:],
+                  "ref_name": ref_name,
+                  "ref_type": ref_type,
+                  "asm": asm
+                  })
     
-#df = pd.DataFrame(rows, colums=cols)
-#df.to_csv('output.csv')
+df = pd.DataFrame(rows)
+df.to_csv('output.csv')
