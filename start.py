9e8e55ba2f09b7c39bafb4f5044866f0b3d4b635
RMO <rmo@sitaniec.pl>
2024-02-06 06:54:58 +0100

    rozgrzebane

--- start.py
+++ start.py
@@ -10,12 +10,11 @@ asm_rows = []
 
 #parsing xml
 # - ProductDef - maszyna 
-# -- InstanceGraph - aktualny zestaw danych (tak domniemam)
-# --- Instance - to wszyskie składowe maszyny z odwołaniami do macierzystych elementów (slidów i złozeń) tu de facto nie wiadomo co jest złożeniem/przeróbką a co częścią (tylko po rozszerzeniu)  
-# --- ProductRevisionView - w tym miejscu to złożenia z listą solidów/part
-# -- ProductRevisionView - w tym miejscy to są solidy bazowe które jako kolejne kopie występują jako Instance (instancje) tu można ukraść pełny link do pliku par
+# -- InstanceGraph - zbiorczy 
+# --- Instance - powołanie wystąpień z definicji (jn) 
+# --- ProductRevisionView - definicje w 2 wersjach solid i assembly
 # -- ProductView - 
-# --- Occurence - czyli gdzie dane (jedno) id występuje ciekawe ale nadmiarowe(?)
+# --- Occurence - wystąpienia definicji
 
 # xmlparse - cała struktura
 fname = "036P_00.00.00.000_Przenosnik_lancuchowy.plmxml"
@@ -30,33 +29,72 @@ maszyna = xmlparse.find('{http://www.plmxml.org/Schemas/PLMXMLSchema}ProductDef/
 skladowe = list(maszyna.findall('{http://www.plmxml.org/Schemas/PLMXMLSchema}Instance'))
 referencje = list(maszyna.findall('{http://www.plmxml.org/Schemas/PLMXMLSchema}ProductRevisionView'))
 
-#  na wyjściu potrzebuję takiej struktury
-#  asm.csv - lista złorzeń w formacie id, nazwa, (id1,id......)
-#  par.csv - lista części (bez złorzeń) w formacie id, nazwa, id_zlozenia
+struktura = []
 
-# jadziem z iteracją po listach iiihaaa, patataj, patataj
-for i in referencje: 
-    # i to złorzenie z nazwą i listą skladowych
-    asm_id = i.attrib["id"] 
-    if asm_id == "thisAsm":
-        mach_name = i.attrib["name"].split("_")
-    asm_name = i.attrib["name"]
-    #childrens to lista elementów w danym (1) złorzeniu
-    # childrens = [12,123,124] 
-    childrens = list(i.attrib["instanceRefs"].split(" "))
-    for m in childrens:  
-        # m to kolejny element w złorzeniu 
-        for t in skladowe: # szukamy w skladowych odpowiadających m numerów id
-            # t to tablica de facto z nazwą i id do powiązania z m
-            if m == t.attrib["id"]: # znaleziono parkę 
-               par_per_asm = 1
-               par_name = t.attrib["name"].rsplit('.',1) #czyszczenie rozszerzenia pliku i nr wystąpienia - .asm: 4
-               if par_name[1].find('asm') == -1: #sprawdź czy to złorzenie czy nie i wystaw flagę - po co nie wiem jeszcze ale czuję, że się przyda
-                    is_asm = 0
-               else:
-                    is_asm = 1
-        # dokładamy wiersz do tablicy
-        asm_rows.append({"maszyna": mach_name[0]  ,"asm_name": asm_name, "name": par_name[0], "biblioteka": par_name[0], "par_per_asm": par_per_asm, "is_asm": is_asm})
+def znajdzAsm(nrId):
+    for i in referencje:
+        if i.attrib['id'] == nrId:
+            r_name = i.attrib['name']
+            is_asm = 0 if i.attrib["type"] == "solid" else 1
+            if is_asm == 0:
+                return(r_name, is_asm)
+            else:
+                 r_list = list(i.attrib["instanceRefs"].split(" ")) #lista składowych
+                 for m in r_list
+
+
+mach_name = 
+
+for i in skladowe:
+    if i.attrib["id"] == "thisAsm":
+        mach_name = i.attrib["name"]
+        split_mach_name = mach_name.split("_")
+        el_list = list(i.attrib["instanceRefs"].split(" ")) #tą listę
+    for t in el_list:
+            
+        
+        
+        #is_asm = 0 if i.attrib["type"] == "solid" else 
+       
+       
+        # asm_rows.append({
+        #     "maszyna": split_mach_name[0], 
+        #     "asm_name": mach_name,
+        #     "name": par_name[0],
+        #     "biblioteka": par_name[0],
+        #     "par_per_asm": par_per_asm,
+        #     "is_asm": is_asm
+        # })
+
+
+# # jadziem z iteracją po listach iiihaaa, patataj, patataj
+# for i in referencje: 
+#     # "i" złorzenie z nazwą i listą skladowych
+#     ref_id = i.attrib["id"] 
+#     ref_name = i.attrib["name"]
+#     ref_type = i.attrib["type"]
+#     # tutaj sobie kradnę nazwę maszyny
+#     if ref_id == "thisAsm":
+#         mach_name = ref_name.split("_")
+    
+ 
+
+#     #childrens to lista elementów w danym (1) złorzeniu
+#     # childrens = [12,123,124] 
+#     childrens = list(i.attrib["instanceRefs"].split(" "))
+#     for m in childrens:  
+#         # m to kolejny element w złorzeniu 
+#         for t in skladowe: # szukamy w skladowych odpowiadających m numerów id
+#             # t to tablica de facto z nazwą i id do powiązania z m
+#             if m == t.attrib["id"]: # znaleziono parkę 
+#                par_per_asm = 1
+#                par_name = t.attrib["name"].rsplit('.',1) #czyszczenie rozszerzenia pliku i nr wystąpienia - .asm: 4
+#                if par_name[1].find('asm') == -1: #sprawdź czy to złorzenie czy nie i wystaw flagę - po co nie wiem jeszcze ale czuję, że się przyda
+#                     is_asm = 0
+#                else:
+#                     is_asm = 1
+#         # dokładamy wiersz do tablicy
+#         asm_rows.append({"maszyna": mach_name[0]  ,"asm_name": asm_name, "name": par_name[0], "biblioteka": par_name[0], "par_per_asm": par_per_asm, "is_asm": is_asm})
 
 
 #print(asm_rows)
