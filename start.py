b52f800b63f75637eb7f6e94b47eba99a52ba60b
Robert <robert.moteka@gmail.com>
2024-02-08 01:55:50 +0100

    nocka

--- start.py
+++ start.py
@@ -3,17 +3,15 @@ import pyarrow as pa
 import pandas as pd 
 
 
-""" asm_ to tabela złorzeń i części brana z Occurence
-asm_name to nazwa nadrzędnego zlożenia (jak główne to null)
-name to nazwa zlozenia lub czesci bez suffixu
-is_asm to dodatkowe info że to zlozenie
+""" asm_ to tabela złorzeń brana z Occurence
+
 """
-asm_cols = ["maszyna", "parent_name", "name", "biblioteka",  ]
+asm_cols = ["mach_id", "asm_id", "asm_name", "par_id", "ile"]
 asm_rows = []
 
 # biblioteka chyba ciurkiem z Instance
-lib_cols = []
-lib_rows = []
+par_cols = ["id", "name"]
+par_rows = []
 
 """parsing xml
 <ProductDef> - maszyna 
@@ -53,16 +51,42 @@ jeżeli ma occurrenceRefs to jest złożeniem
 occ..R zawiera wszystkie składowe
 """
 
-
-count = 0
+maszyna = pdef.attrib["name"]
+mach_id = maszyna.split("_")[0]
+ile = 0
 for i in occurence:
-    if ("visible" in i.attrib):
-        # czy jest widoczny jeżeli nie to nie ma go
-        if i.attrib["visible"] != "false" :
-            # jest zlożeniem
-            if ("occurrenceRefs" in i.attrib):
-               print("do nothing")
+    if ("visible" in i.attrib) or not("name" in i.attrib): # żeby nie sypało błędami jakby z jakiegoś powodu nie było ustawione
+        if not("visible" in i.attrib) or i.attrib["visible"] != "false"    : #jest widoczny (jak nie to olewam go)
+            id = i.attrib["id"] # id rozpatrywanego elementu
+            ################# ANALIZA ZŁOŻENIA #######################
+            if ("occurrenceRefs" in i.attrib): # jest zlożeniem                
+                refs_list = list(i.attrib["occurrenceRefs"].split(" ")) #tymczasowa lista childrenów                
+                if ("name" in i.attrib): #przypisanie nazwy zlozenia jeżeli ma name
+                   asm_name = i.attrib["name"].rsplit('.',1)[0] #czyszczenie rozszerzenia pliku i nr wystąpienia - .asm: 4
+                   print(asm_name)
+                else: #przypisanie nazwy zlozenia (id maszyny) dla root-a
+                   asm_name = maszyna
+                   print(asm_name)
+                # iteracja po liście składowych 
+                # id to nr składowej                
+                for p in refs_list: #asm_cols =  ["par_id", "mach_id", "asm_id", "asm_name", "par_name" ]
+                    asm_rows.append({"par_id": p , "mach_id": mach_id, "asm_id": id, "asm_name": asm_name})
+
+            ################# ANALIZA ELEMENTÓW #######################
+            else: # nie jest zlozeniem
+                par_rows.append({"id": id ,"name": i.attrib["name"].rsplit('.',1)[0]})
+
+    ile = ile + 1
+print("Przebadano: " + str(ile) + " rekordów.")
+print("Zapisano: " + str(len(asm_rows)) + " części zadeklarowanych w złożeniach.")
+print("Zapisano: " + str(len(par_rows)) + " części.")
 
 
-    count = count + 1
-print(count)
+asm_f = pd.DataFrame(asm_rows, columns=asm_cols, index=None)
+asmg_f = asm_f.groupby(['asm_id'])['ile'].count()
+asmg_f.to_csv(fname + '_asm.csv')
+par_f = pd.DataFrame(par_rows, columns=par_cols, index=None)
+par_f.to_csv(fname + '_par.csv')
+
+df1 = asm_f.merge(par_f, left_on="par_id", right_on="id")
+df1.to_csv(fname + '_mrg.csv')
